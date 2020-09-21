bdb2821598fcb78e61d505c83409b461c7dec38f
Luis Pedro Morales <luismorarod98@gmail.com>
2020-09-21 00:57:11 -0600

    Add main

---
+++ Tarea 1/cmake-build-debug/CMakeFiles/Tarea_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_1.dir/Vertex.cpp.o"
+  "CMakeFiles/Tarea_1.dir/VertexList.cpp.o"
+  "CMakeFiles/Tarea_1.dir/main.cpp.o"
+  "Tarea_1"
+  "Tarea_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tarea 1/main.cpp
@@ -0,0 +1,45 @@
+#include <QApplication>
+#include <QWidget>
+#include <iostream>
+
+#include "Vertex.h"
+#include "VertexList.h"
+
+int main(int argc, char *argv[]) {
+/*
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    window.resize(250, 150);
+    window.setWindowTitle("Simple example");
+    window.show();
+
+    return app.exec();*/
+    Vertex vertex_a ("A");
+    Vertex vertex_b ("B");
+    Vertex vertex_c ("C");
+
+    Vertex *  pVertexA = &vertex_a;
+    Vertex *  pVertexB = &vertex_b;
+    Vertex *  pVertexC = &vertex_c;
+
+    VertexList list (pVertexA);
+    std::cout<< "New linked list."<<std::endl;
+    std::cout<<"First element: "<< list.GetHead()->GetName()<<std::endl;
+    std::cout<< "Size: "<<list.GetSize()<<std::endl<<std::endl;
+
+    list.InsertEnd(pVertexB);
+    std::cout<<"Insertion"<<std::endl;
+    std::cout<<"Last element: "<<list.GetLast()->GetName()<<std::endl;
+    std::cout<< "Size: "<<list.GetSize()<<std::endl<<std::endl;
+
+    list.InsertEnd(pVertexC);
+    std::cout<<"Iteration"<<std::endl;
+    Vertex* ptr = list.GetHead();
+    for (int i = 0; i< list.GetSize(); i++){
+        std::cout<<ptr->GetName()<<std::endl;
+        ptr = ptr->GetPNext();
+    }
+    return 0;
+}
